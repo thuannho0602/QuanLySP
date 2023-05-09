31d1ebca13ca81315ceea754f6cf865aede0dc3b
thuannguyen <thuannho0602@gmail.com>
2023-05-09 19:38:57 +0700

    databaseEF

---
+++ QYSANPHAM/QLSANPHAM.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using QLSANPHAM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSANPHAM.DataAccess
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public virtual DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ QYSANPHAM/QLSANPHAM.Entity/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSANPHAM.Entity
+{
+    [Table("Product")]
+    public class Product
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+        public string ProductDescription { get; set; }
+        public string Origin { get; set; }
+        public string Trademark { get; set; }
+        public string Memory { get; set; }
+        public string TheBattery { get; set; }
+        public decimal Price { get; set; }
+        public string Promotion { get; set; }
+        public int TheRemainingAmount { get; set; }
+    }
+}
